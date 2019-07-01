6033717261af32b6f7871cffe719e200b28eb833
wqzwh <wangziqihappy@163.com>
2019-07-01 20:53:35 +0800

    refactor:删除冗余引入包文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import sagas from './rootSagas'
 import reducers from './rootReducers'
 
 const sagaMiddleware = createSagaMiddleware()
